7485eae3decd51febc056f0314a66b807f3b5253
Matthew Blackley <123616859+MattBlack9@users.noreply.github.com>
2024-09-10 10:35:47 -0500

    Update NewtonsMethod.R

--- NewtonsMethod.R
+++ NewtonsMethod.R
@@ -12,21 +12,24 @@ NewtonsMethod <- function(f, fprime, fdoubleprime, x0, nIter){
   # Initialize storage for iterations and function values
   fvec = rep(f(x0), nIter + 1)
   xvec = rep(x0, nIter + 1)
+  fvec[1] <- f(x0)
+  xvec[1] <- x0
   
   # Perform Newton's update for nIter iterations
   for (i in 1:nIter){
     # At each iteration, update current value of x and save the new function value
     # [ToDo] fill in
     # Newton's method update
+    xvec[i + 1] <- xvec[i] - (fprime(xvec[i]) / fdoubleprime(xvec[i]))
 
     # Function value
+    fvec[i + 1] <- f(xvec[i + 1])
 
   }
   
   # Return the vector of x values, as well as the vector of function values across iterations, including the starting point (both length nIter + 1)
   return(list(xvec = xvec, fvec  = fvec))
 }
-
 # Newton's method with the fixed number of iterations, function argument is a scalar
 #####################################################################################
 
